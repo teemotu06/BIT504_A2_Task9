27fa327d0f289c6b25e7e6504ea6e5d9efced026
teemotu06 <tmotu06@gmail.com>
2025-01-22 10:25:03 +0900

    Initial commit - added to main class

---
+++ BIT504_A2_Task9/src/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi, GitHub!");
+        System.out.println("This is the version Control Demonstration/test");
+    }
+}
